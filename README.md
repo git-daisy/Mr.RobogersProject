58de68acad88d581a8cd1c5b6ad2618d6615d149
Daisy Collins <daisydcollins12@gmail.com>
2021-07-04 18:40:05 -0700

    All values only returning 0, continue debug

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Mr.RobogersProject
+
+Describe: beepBoop()
+
+Test: "It should return with a 0 if the number 0 is inputted"
+Expect: (beepBoop(0).toEqual(0); 
+
+Test: "It should return all numbers coming before the number in which the user inputs" 
+Expect: (beepBoop(5).toEqual(0,1,2,3,4,5))
+
+Test: "It should replace all numbers containing '3' with "Won't you be my neighbor?"
+Expect: (beepBoop(5).toEqual("0","1","2","Won't you be my neighbor?","4","5"))
+
+Test: " It should replace all numbers containing 2 with "Boop"
+Expect: (beepBoop(5).toEqual("0","1","Boop","Won't you be my neighbor?","4","5"))
+
+Test: "It should replace all numbers containing 1 with "Beep"
+Expect: (beepBoop(5).toEqual("0","Beep","Boop","Won't you be my neighbor?"))
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// UI Logic 
+let returnString = ""
+
+function beepBoop(input) {
+  returnString = "" 
+  for (i = 0; i <= input; i++) {
+    if ((i + '').indexOf('3') > -1) {
+      returnString += "Won't you be my neighbor?"
+    } else if ((i + '').indexOf('2') > -1) {
+      returnString += "Boop"
+    } else if ((i + '').indexOf('1') > -1) {
+      returnString += "Beep"
+    } else {
+    returnString += i && "" 
+    }
+
+    if (returnString != "") {
+      return returnString;
+    }
+  }
+}
+// Business Logic 
+
+$(document).ready(function() {
+  $("#robogers").submit.(function(event) {
+    const input = parseInt($("input#number").val());
+    console.log(beepBoop(input));
+    let returnString = beepBoop(input);
+    event.preventDefault();
+    $("#output").show();
+  });
+})
